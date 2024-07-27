feab929759f1a43a9e75ad8094db38c5905d7b5b
jaennil <jaennil@yandex.ru>
2024-07-27 19:06:40 +0300

    feat: qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,13 @@
+c.editor.command = ['alacritty', '-e', 'nvim', '{file}']
+
+config.bind('<Alt++>', 'tab-focus 1')
+config.bind('<Alt+[>', 'tab-focus 2')
+config.bind('<Alt+{>', 'tab-focus 3')
+config.bind('<Alt+(>', 'tab-focus 4')
+config.bind('<Alt+&>', 'tab-focus 5')
+config.bind('<Alt+=>', 'tab-focus 6')
+config.bind('<Alt+)>', 'tab-focus 7')
+config.bind('<Alt+}>', 'tab-focus 8')
+config.bind('<Alt+]>', 'tab-focus -1')
+
+config.load_autoconfig(False)
