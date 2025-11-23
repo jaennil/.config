f6010152fc3e6d9deef16c182ebed0a2538d0b7e
jaennil <jaennil@yandex.ru>
2025-11-23 22:32:50 +0300

    feat: idk ssh agent stuff

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,7 @@ fish_better_vi_key_bindings
 
 set -gx PATH $PATH ~/.local/share/nvim/mason/bin
 set -gx BROWSER zen-browser
+
+if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
+    exec ssh-agent startx
+end
