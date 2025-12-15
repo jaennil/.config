985d27ad2998dc4f10cf8e9d59592c99e6b5809c
jaennil <jaennil@yandex.ru>
2025-12-16 00:32:40 +0300

    feat(fish): add shorthand commit syntax to g function

--- fish/functions/g.fish
+++ fish/functions/g.fish
@@ -1,3 +1,7 @@
 function g --wraps=git --description 'alias g=git'
-  git $argv
+  if test (count $argv) -ge 2; and test "$argv[1]" = "c" -o "$argv[1]" = "commit"
+    git commit -m "$argv[2..-1]"
+  else
+    git $argv
+  end
 end
